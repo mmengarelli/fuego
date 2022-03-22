848dcec329b93f57768cbabffd476126fe834c53
mike.m <mengam01@gmail.com>
2022-03-22 15:01:51 -0400

    update build

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 echo "Running unit tests ..."
-./run-unittests.sh
-./run-pytest.sh
-./run-nutter.sh
+
+. ${__dir}/run-unittests.sh
+. ${__dir}/run-pytest.sh
+. ${__dir}/run-nutter.sh
+
